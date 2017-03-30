e728f11ea92ccf62be8a525f2b05c11e6518a727
ScottDowne <scott.downe@gmail.com>
2017-03-30 14:02:51 -0400

    Sheets integration.

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,34 @@
 $(document).ready(function() {
-		
-		});
+
+  function addToSheets(field, callback) {
+    var http = new XMLHttpRequest();
+    var url = "/api/sheets/add";
+    callback = callback || function() {};
+
+    http.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        callback("success");
+      }
+    };
+
+    http.open("POST", url, true);
+    http.setRequestHeader("Content-type", "application/json");
+    http.send(JSON.stringify({field: field}));
+  }
+
+  function readFromSheets(callback) {
+    var http = new XMLHttpRequest();
+    var url = "/api/sheets/read";
+    callback = callback || function() {};
+
+    http.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        var result = JSON.parse(this.responseText);
+        callback(result);
+      }
+    };
+
+    http.open("GET", url, true);
+    http.send();
+  }
+});
