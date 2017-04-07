a2da4a761031ce4cbcba07b1a893687ac65ed938
ScottDowne <scott.downe@gmail.com>
2017-04-07 16:58:29 -0400

    waiting and scroll fix

--- public/js/main.js
+++ public/js/main.js
@@ -48,17 +48,43 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   var timeOut;
+  var paused = false;
   var outputContainer = document.querySelector('.output-container');
   var inputElement = document.querySelector('.input');
+
+  function waiting(on) {
+    if (on) {
+      inputElement.disabled = true;
+      var rowElement = document.createElement('div');
+      var firstChild = outputContainer.firstChild;
+      rowElement.innerHTML = "<span>.</span><span>.</span><span>.</span>";
+      rowElement.classList.add("waiting");
+      if (!firstChild) {
+        outputContainer.appendChild(rowElement);
+      } else {
+        outputContainer.insertBefore(rowElement, outputContainer.firstChild);
+      }
+    } else {
+      inputElement.disabled = false;
+      var waitingElement = document.querySelector(".waiting");
+      if (waitingElement && waitingElement.parentNode) {
+        waitingElement.parentNode.removeChild(waitingElement);
+      }
+    }
+  }
+
   inputElement.addEventListener('keydown', function(e){
     var value = inputElement.value.trim().slice(0, 140);
     if(value && e.keyCode === 13) {
 
+      waiting(true);
       inputElement.value = '';
       document.querySelector('.thankyou').classList.add('show');
       clearTimeout(timeOut);
+      timeOut = null;
       updateOutput(function() {
         addToSheets(value, function() {
+          waiting(false);
           var rowElement = document.createElement('div');
           var firstChild = outputContainer.firstChild;
           rowElement.textContent = value;
@@ -67,6 +93,7 @@ document.addEventListener('DOMContentLoaded', function() {
           } else {
             outputContainer.insertBefore(rowElement, outputContainer.firstChild);
           }
+          timeOut = window.setTimeout(updateOutputTimeout, 4000);
         });
       });
     }
@@ -75,14 +102,26 @@ document.addEventListener('DOMContentLoaded', function() {
   inputElement.focus();
   var previousGuid = "";
 
+  function updateOutputTimeout() {
+    updateOutput(function() {
+      timeOut = window.setTimeout(updateOutputTimeout, 4000);
+    });
+  }
+
   function updateOutput(callback){
     callback = callback || function() {};
     var cacheElement = document.createElement("div");
+    var waitingElement;
     readFromSheets(function(data){
 
       var rows = data.rows;
       var guid = data.guid;
-      if (guid !== previousGuid) {
+      if (guid !== previousGuid && !paused) {
+        var waitingElement = document.querySelector(".waiting");
+
+        if (waitingElement) {
+          cacheElement.appendChild(waitingElement);
+        }
         rows.forEach(function(row){
           var rowElement = document.createElement('div');
           rowElement.textContent = row.field;
@@ -92,12 +131,19 @@ document.addEventListener('DOMContentLoaded', function() {
         previousGuid = guid;
       }
       callback();
-      timeOut = window.setTimeout(updateOutput, 4000);
     });
   }
 
-  timeOut = window.setTimeout(updateOutput);
+  timeOut = window.setTimeout(updateOutputTimeout);
 
+  window.addEventListener("scroll", function() {
+    var scrollY = window.scrollY;
+    if (scrollY === 0) {
+      paused = false;
+    } else {
+      paused = true;
+    }
+  });
 
   window.addToSheets = addToSheets;
   window.readFromSheets = readFromSheets;
